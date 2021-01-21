e87aa4cb3893592db114159fe8681cfca54f1043
OlgaSe <olga.tka4eva@gmail.com>
2021-01-20 22:26:08 -0800

    added create method to the videos controller, that adds selected videos to the db

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -21,6 +21,35 @@ class VideosController < ApplicationController
       )
   end
 
+  def create
+    errors = []
+
+    params[:videos].each do |video_params|
+      video = Video.create(
+          title: video_params[:title],
+          overview: video_params[:overview],
+          release_date: video_params[:release_date],
+          image_url: video_params[:image_url],
+          external_id: video_params[:external_id],
+          inventory: 1,
+          )
+
+      if !video.valid?
+        errors << video.errors.messages
+      end
+
+    end
+
+    # videos_params[:videos].each do |video_params|
+    #   Video.create(video_params)
+    # end
+
+    render(
+        status: :ok,
+        json: {errors: errors}
+    )
+  end
+
   private
 
   def require_video
@@ -28,5 +57,9 @@ class VideosController < ApplicationController
     unless @video
       render status: :not_found, json: { errors: { title: ["No video with title #{params["title"]}"] } }
     end
+  end
+
+  def videos_params
+    params.permit(videos: [:title, :overview, :release_date, :image_url, :external_id])
   end
 end
